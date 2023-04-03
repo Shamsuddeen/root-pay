1a8e4fcc9c782316192206bbfa3c313df05382d5
Shamsuddeen Omacy <omacy@Shamsuddeens-MacBook-Air.local>
2023-04-03 14:53:12 +0100

    Card Model

---
+++ Model/Card.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    cardId: {
+        type: String,
+        required: true
+    },
+    customerId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    pan: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    expiry: String,
+    brand: String,
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    channels: [{
+        atm: {
+            type: Boolean,
+            required: true,
+            default: true
+        },
+        pos: {
+            type: Boolean,
+            required: true,
+            default: true
+        },
+        web: {
+            type: Boolean,
+            required: true,
+            default: true
+        },
+        mobile: {
+            type: Boolean,
+            required: true,
+            default: true
+        }
+    }],
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('cards', cardSchema);
