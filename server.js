8a936733e1ce46da942d4f266486ff8447c6568d
Shamsuddeen Omacy <omacy@Shamsuddeens-MacBook-Air.local>
2023-04-04 08:47:37 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express       = require('express');
+const dotenv        = require('dotenv');
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const cors          = require('cors');
+const morgan        = require('morgan');
+const cookieParser = require('cookie-parser');
+
+// Load ENV vars
+dotenv.config({ path: './.env'});
+// Initialise the app
+const connectDB = require('./Config/db');
+
+// Connect to database
+connectDB();
+const app = express();
+
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// Cookie parser
+app.use(cookieParser());
+app.use(cors());
+
+var port = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('Hello from Root.Cards'));
+
+// Development logging
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log(`Running Root.Cards in ${process.env.NODE_ENV} mode on port ` + port);
+});
